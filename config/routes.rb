bf1c30c0605378e5209dbbc316fea428635151d9
RenatoDaSilva <renato.katia@gmail.com>
2017-09-29 23:40:33 -0300

    modelo usuario

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   root 'static_pages#home'
   get  '/noticias', to: 'static_pages#noticias'
   get  '/biblioteca', to: 'static_pages#biblioteca'
